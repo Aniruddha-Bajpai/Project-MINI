53ad0a57bb7969574f0ffd3d6768b46c9b7341b4
Aniruddha Bajpai <bajpaianirudh03@gmail.com>
2022-11-20 03:47:05 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-//SIDEBAR
+// SIDEBAR
 const menuItems = document.querySelectorAll(".menu-item");
-
-//MESSAGES
+// MESSAGES
 const messagesNotification = document.querySelector("#messages-notification");
 const messages = document.querySelector(".messages");
-const message = messages.querySelectorAll(".message");
+const message = document.querySelectorAll(".message");
 const messageSearch = document.querySelector("#message-search");
-
-// THEME
+//THEME
 const theme = document.querySelector("#theme");
 const themeModal = document.querySelector(".customize-theme");
-const fontsizes = document.querySelectorAll(".choose-size span");
+const fontSizes = document.querySelectorAll(".choose-size span");
 var root = document.querySelector(":root");
 const colorPalette = document.querySelectorAll(".choose-color span");
+const Bg1 = document.querySelector(".bg-1");
+const Bg2 = document.querySelector(".bg-2");
+const Bg3 = document.querySelector(".bg-3");
 
-//============SIDEBAR================
-
-//remove active class from all menu items
-const changeActiveItems = () => {
+// remove active class from all menu items
+const changeActiveItem = () => {
   menuItems.forEach((item) => {
     item.classList.remove("active");
   });
 };
-
+//  ================= SIDEBAR ================
 menuItems.forEach((item) => {
   item.addEventListener("click", () => {
-    changeActiveItems();
+    changeActiveItem();
     item.classList.add("active");
     if (item.id != "notifications") {
       document.querySelector(".notifications-popup").style.display = "none";
@@ -38,24 +37,23 @@ menuItems.forEach((item) => {
   });
 });
 
-//================MESSAGES=================
-//search chats
+// ================== MESSAGES =============
+// searches chats
 const searchMessage = () => {
   const val = messageSearch.value.toLowerCase();
-  message.forEach((user) => {
-    let name = user.querySelectorAll("h5").textContent.toLowerCase();
+  message.forEach((chat) => {
+    let name = chat.querySelector("h5").textContent.toLowerCase();
     if (name.indexOf(val) != -1) {
-      user.style.display = "flex";
+      chat.style.display = "flex";
     } else {
-      user.style.display = "none";
+      chat.style.display = "none";
     }
   });
 };
-
-//search chat
+// search chat
 messageSearch.addEventListener("keyup", searchMessage);
 
-//highlight message card when messages menu item is clicked
+// highlight messages card when messages menu item is clicked
 messagesNotification.addEventListener("click", () => {
   messages.style.boxShadow = "0 0 1rem var(--color-primary)";
   messagesNotification.querySelector(".notifications-count").style.display =
@@ -65,81 +63,72 @@ messagesNotification.addEventListener("click", () => {
   }, 2000);
 });
 
-//THEME/DISPLAY CUSTOMIZATION
+// THEME/DISPLAY CUSTOMIZATIONS
 
-//opens modal
+// open-modal-function
 const openThemeModal = () => {
   themeModal.style.display = "grid";
 };
-
-//closes modal
+//  close-modal-function
 const closeThemeModal = (e) => {
   if (e.target.classList.contains("customize-theme")) {
     themeModal.style.display = "none";
   }
 };
-
-//close modal
+// close event listener
 themeModal.addEventListener("click", closeThemeModal);
-
+// open event listener
 theme.addEventListener("click", openThemeModal);
 
-//==============FONTS=================
-
-//remove active calls from spans or font size selectors
+//
 const removeSizeSelector = () => {
-  fontsizes.forEach((size) => {
+  fontSizes.forEach((size) => {
     size.classList.remove("active");
   });
 };
-
-fontsizes.forEach((size) => {
+fontSizes.forEach((size) => {
   size.addEventListener("click", () => {
+    let fontSize;
     removeSizeSelector();
-    let fontsize;
-    size.classList.toggle("active");
-
+    size.classList.toggle("active"); // remove an existing from the list and returns false
     if (size.classList.contains("font-size-1")) {
-      fontsize = "10px";
-      root.style.setProperty("----sticky-top-left", "5.4rem");
-      root.style.setProperty("----sticky-top-right", "5.4rem");
+      fontSize = "10px";
+      root.style.setProperty("--sticky-top-left", "5.4rem");
+      root.style.setProperty("--sticky-top-right", "5.4rem");
     } else if (size.classList.contains("font-size-2")) {
-      fontsize = "13px";
-      root.style.setProperty("----sticky-top-left", "5.4rem");
-      root.style.setProperty("----sticky-top-right", "-7rem");
+      fontSize = "13px";
+      root.style.setProperty("--sticky-top-left", "5.4rem");
+      root.style.setProperty("--sticky-top-right", "-7rem");
     } else if (size.classList.contains("font-size-3")) {
-      fontsize = "16px";
-      root.style.setProperty("----sticky-top-left", "-2rem");
-      root.style.setProperty("----sticky-top-right", "-17rem");
+      fontSize = "16px";
+      root.style.setProperty("--sticky-top-left", "-2rem");
+      root.style.setProperty("--sticky-top-right", "-17rem");
     } else if (size.classList.contains("font-size-4")) {
-      fontsize = "19px";
-      root.style.setProperty("----sticky-top-left", "-5rem");
-      root.style.setProperty("----sticky-top-right", "-25rem");
+      fontSize = "19px";
+      root.style.setProperty("--sticky-top-left", "-5rem");
+      root.style.setProperty("--sticky-top-right", "-25rem");
     } else if (size.classList.contains("font-size-5")) {
-      fontsize = "22px";
-      root.style.setProperty("----sticky-top-left", "-12rem");
-      root.style.setProperty("----sticky-top-right", "-35rem");
+      fontSize = "22px";
+      root.style.setProperty("--sticky-top-left", "-12rem");
+      root.style.setProperty("--sticky-top-right", "-35rem");
     }
-
-    //change font size of the root html element
-    document.querySelector("html").style.fontsize = fontsize;
+    // change font size of the root html element
+    document.querySelector("html").style.fontSize = fontSize;
   });
 });
 
-//remove active calls from colors
+// remove active class from colors;
 const changeActiveColorClass = () => {
   colorPalette.forEach((colorPicker) => {
     colorPicker.classList.remove("active");
   });
 };
 
-// change primary colors
+// change primary color
 colorPalette.forEach((color) => {
   color.addEventListener("click", () => {
-    let primary;
-    //remove active calls from colors
+    let primaryHue;
     changeActiveColorClass();
-
     if (color.classList.contains("color-1")) {
       primaryHue = 252;
     } else if (color.classList.contains("color-2")) {
@@ -152,7 +141,52 @@ colorPalette.forEach((color) => {
       primaryHue = 202;
     }
     color.classList.add("active");
-
     root.style.setProperty("--primary-color-hue", primaryHue);
   });
+});
+
+// theme background values
+let lightcolorlightness;
+let whitecolorlightness;
+let darkcolorlightness;
+// change background function
+const changeBG = () => {
+  root.style.setProperty("--light-color-lightness", lightcolorlightness);
+  root.style.setProperty("--white-color-lightness", whitecolorlightness);
+  root.style.setProperty("--dark-color-lightness", darkcolorlightness);
+};
+Bg1.addEventListener("click", () => {
+  // add active class
+  Bg1.classList.add("active");
+  // remove active class from the others
+  Bg2.classList.remove("active");
+  Bg3.classList.remove("active");
+  // this would remove the customised changes from local storage
+  window.location.reload();
+});
+
+Bg2.addEventListener("click", () => {
+  darkcolorlightness = "95%";
+  whitecolorlightness = "20%";
+  lightcolorlightness = "15%";
+
+  // add active class
+  Bg2.classList.add("active");
+  // remove active class from the others
+  Bg1.classList.remove("active");
+  Bg3.classList.remove("active");
+  changeBG();
+});
+
+Bg3.addEventListener("click", () => {
+  darkcolorlightness = "95%";
+  whitecolorlightness = "10%";
+  lightcolorlightness = "0%";
+
+  // add active class
+  Bg3.classList.add("active");
+  // remove active class from the others
+  Bg1.classList.remove("active");
+  Bg2.classList.remove("active");
+  changeBG();
 });
